4147d73ec22d250cb278c94a51f0ca8cc66d6f05
aviadhahami <aviadhahami@gmail.com>
2014-07-28 18:23:19 +0300

    Carousel finished

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
     console.log("JQ LOADED");
+    fakeCarousel();
+
+    function fakeCarousel() {
+        console.log("in fun");
+
+        $('#carImg').fadeOut(500, function () {
+            $('#carImg').attr("src", "img/headerPhoto1.png");
+            $('#carImg').fadeIn(500);
+        });
+
+        window.setTimeout(function () {
+            $('#carImg').fadeOut(500, function () {
+                $('#carImg').attr("src", "img/headerPhoto2.png");
+                $('#carImg').fadeIn(500);
+            });
+        }, 30000);
+        window.setTimeout(function () {
+            $('#carImg').fadeOut(500, function () {
+                $('#carImg').attr("src", "img/headerPhoto3.png");
+                $('#carImg').fadeIn(500);
+            });
+        }, 6000);
+        windo0w.setTimeout(function () {
+            $('#carImg').fadeOut(500, function () {
+                $('#carImg').attr("src", "img/headerPhoto4.png");
+                $('#carImg').fadeIn(500);
+            });
+        }, 90000);
+        window.setTimeout(function () {
+            $('#carImg').fadeOut(500, function () {
+                $('#carImg').attr("src", "img/headerPhoto5.png");
+                $('#carImg').fadeIn(500);
+            });
+        }, 120000);
+        setTimeout(fakeCarousel, 150000);
+    }
+
+
 });
