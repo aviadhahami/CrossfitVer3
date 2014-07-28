74eb56e1a31c01c5013a693a605cfb5f91ddbfc3
aviadhahami <aviadhahami@gmail.com>
2014-07-28 19:09:33 +0300

    DONE WITH THE FUCKING CAROUSEL !

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,9 +1,51 @@
+"use strict";
 $(document).ready(function () {
     console.log("JQ LOADED");
-    fakeCarousel();
+    // fakeCarousel();
+    setInterval(function () {
+        var i = $('#carImg').attr("src").charAt($('#carImg').attr("src").length - 5);
+        var index = parseInt(i);
+        //console.log(index);
+        if (index === 5) {
+            index = 1;
+        } else {
+            index++;
+        }
+        i = index.toString();
+        // console.log($('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png");
+
+
+        var finalStr = $('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png";
+
+        $('#carImg').fadeOut(500, function () {
+            $('#carImg').attr('src', finalStr);
+            $('#carImg').fadeIn(500);
+        });
+
+    }, 30000);
+
+
+    function fakeCarouselTwo() {
+        var i = $('#carImg').attr("src").charAt($('#carImg').attr("src").length - 5);
+        var index = parseInt(i);
+        console.log(index);
+        if (index === 5) {
+            i = "1";
+        } else {
+            index++;
+            i = index.toString();
+            console.log($('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png");
+            var finalStr = $('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png";
+            $('#carImg').attr('src', finalStr);
+        }
+    }
+
+
+
+
+
 
     function fakeCarousel() {
-        console.log("in fun");
 
         $('#carImg').fadeOut(500, function () {
             $('#carImg').attr("src", "img/headerPhoto1.png");
@@ -21,8 +63,8 @@ $(document).ready(function () {
                 $('#carImg').attr("src", "img/headerPhoto3.png");
                 $('#carImg').fadeIn(500);
             });
-        }, 6000);
-        windo0w.setTimeout(function () {
+        }, 60000);
+        window.setTimeout(function () {
             $('#carImg').fadeOut(500, function () {
                 $('#carImg').attr("src", "img/headerPhoto4.png");
                 $('#carImg').fadeIn(500);
