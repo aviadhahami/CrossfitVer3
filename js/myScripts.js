684e474e54acdf1fbf9b42009639bbf3696a5ddf
aviadhahami <aviadhahami@gmail.com>
2014-08-03 01:55:05 +0300

    Revert "Ori's update"
    This reverts commit adb4d630d9c479dc133d8d0c6461ec5eb0a2ea7c.

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,34 +1,35 @@
 "use strict";
-$(document).ready(function() {
-    $('body').css('display', 'none');
-    $('body').fadeIn(1000);
+$(document).ready(function () {
+        $('body').css('display', 'none');
+        $('body').fadeIn(1000);
 
-    console.log("JQ LOADED");
-    // fakeCarousel();
-    setInterval(function() {
-        var i = $('#carImg').attr("src").charAt($('#carImg').attr("src").length - 5);
-        var index = parseInt(i);
-        //console.log(index);
-        if (index === 5) {
-            index = 1;
-        } else {
-            index++;
-        }
-        i = index.toString();
-        var finalStr = $('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png";
-        $('#carImg').animate({
-            opacity: 0
-        }, 500, function() {
-            $('#carImg').attr('src', finalStr);
+        console.log("JQ LOADED");
+        // fakeCarousel();
+        setInterval(function () {
+            var i = $('#carImg').attr("src").charAt($('#carImg').attr("src").length - 5);
+            var index = parseInt(i);
+            //console.log(index);
+            if (index === 5) {
+                index = 1;
+            } else {
+                index++;
+            }
+            i = index.toString();
+            var finalStr = $('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png";
+
             $('#carImg').animate({
-                opacity: 1
-            }, 500);
+                opacity: 0
+            }, 500, function () {
+                $('#carImg').attr('src', finalStr);
+                $('#carImg').animate({
+                    opacity: 1
+                }, 500);
 
-        });
+            });
 
-    }, 30000);
+        }, 30000);
 
-   
+
 
 
 });
--- landing.php
+++ landing.php
@@ -1,50 +1,47 @@
-
+﻿ 
 <div class="carousel">
-             <div class="container">
-           
-            <div class="span_3_of_3">
-                
-                <div class="overlayText">
-                    <h1 id="title">INSPERATIONAL TEXT LIKE MOJO</h1>
-                    <h2 id="title">This is version #2 of this website</h2>
-                    <button id="overlay" class="buttontext" onclick='alert("clicked");'>FIND OUT NOW</button>
-                    <br>
-                    <br>
-                    <br>
-                    
-                </div>
-               
-          
-         <img alt="pic 1" id="carImg" class="carImg" src="(<?php bloginfo('template_url'); ?>/img/headerPhoto1.png" />
-            </div></div> </div>
-            <div class="foot">
-                <div class="section">
-                    <div class="col_cus span_1_of_4_cus">
-                        <img style="background-image:url('(<?php bloginfo('template_url'); ?>/img/mpic1.jpg');" class="footImg" /><br>
-                        <p class="blogsubtext">
-                             <span class="introtext2">בין השירים הישראלים הבודדים</span><br>
-                            שאני ישראלי שאתה אוהב?</p>
-                    </div>
-                    <div class="col_cus span_1_of_4_cus">
-                        <img style="background-image:url('(<?php bloginfo('template_url'); ?>/img/mpic2.jpg');" class="footImg" />
-                       
-                        <p class="blogsubtext">
-                             <span class="introtext2">בין השירים הישראלים הבודדים</span><br>
-                            שאני ישראלי שאתה אוהב?</p>
-                    </div>
-                  <div class="col_cus span_1_of_4_cus">
-                        <img style="background-image:url('(<?php bloginfo('template_url'); ?>/img/mpic3.jpg');" class="footImg" />
-                      
-                        <p class="blogsubtext">
-                             <span class="introtext2">בין השירים הישראלים הבודדים</span><br>
-                            שאני ישראלי שאתה אוהב?</p>
-                    </div>
-                    <div class="col_cus span_1_of_4_cus">
-                        <img style="background-image:url('(<?php bloginfo('template_url'); ?>/img/mpic4.jpg');" class="footImg" />
-                      
-                        <p class="blogsubtext">
-                             <span class="introtext2">בין השירים הישראלים הבודדים</span><br>
-                            שאני ישראלי שאתה אוהב?</p>
-                    </div>
-                </div>
+        <img alt="pic 1" id="carImg" class="carImg" src="<?php bloginfo('template_url'); ?>/img/headerPhoto1.png" />
+    </div>
+    <div class="span_3_of_3">
+        <div class="overlayText">
+            <h1 id="title">INSPERATIONAL TEXT LIKE MOJO</h1>
+            <h2 id="title">This is version #2 of this website</h2>
+            <button id="overlay" class="buttontext" onclick='alert("clicked");'>FIND OUT NOW</button>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+    <div class="foot">
+        <div class="section">
+            <div class="col_cus span_1_of_4_cus">
+                <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic1.jpg);" class="footImg" />
+                <br>
+                <p class="blogsubtext">
+                    <span class="introtext2">Some intro text</span>
+                    <br>ניסיון אחת שתיים שלוש</p>
             </div>
+            <div class="col_cus span_1_of_4_cus">
+                <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic2.jpg);" class="footImg" />
+
+                <p class="blogsubtext">
+                    <span class="introtext2">Nigga nigga nigga</span>
+                    <br>ניסיון אחת שתיים שלוש</p>
+            </div>
+            <div class="col_cus span_1_of_4_cus">
+                <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic3.jpg);" class="footImg" />
+
+                <p class="blogsubtext">
+                    <span class="introtext2">EAT ME ALIVE</span>
+                    <br>ניסיון אחת שתיים שלוש</p>
+            </div>
+            <div class="col_cus span_1_of_4_cus">
+                <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic4.jpg);" class="footImg" />
+
+                <p class="blogsubtext">
+                    <span class="introtext2">MOO MOO SAYS THE COW</span>
+                    <br>ניסיון אחת שתיים שלוש</p>
+            </div>
+        </div>
+    </div>
+
