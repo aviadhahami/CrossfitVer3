880f217695f3d0d2c1842b47e30cce57b3aca57c
aviadhahami <aviadhahami@gmail.com>
2014-08-02 20:04:52 +0300

    implemented primate AJAX call
    FUCK THIS SHIT !

--- blog.php
+++ blog.php
@@ -7,18 +7,52 @@
 <div class="col span_1_of_3 ">
     <div class="span_2_of_3 signup">
         <h2>הרשמה לאימונים <hr class="style-one" /></h2>
-        <input type="text" value="" id="name">שם
-        <br />
-        <input type="text" value="" id="phone">טלפון
-        <br />
-        <input type="text" value="" id="mail">אימייל
-        <br />
-        <button id="viewPartButton" onclick="alert('U CLICKED ME');">משתתפים</button>
-        <button id="signButton" onclick="alert('U CLICKED ME');">הרשם לאימון</button>
-        <br />
-        <br />
+        <table id="formTable">
+            <tr>
+                <td>
+                    <input type="text" value="" id="name" />
+                </td>
+                <td>
+                    <label class="formLabel">שם</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" value="" id="phone" />
+                </td>
+                <td>
+                    <label class="formLabel">טלפון</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" value="" id="mail" />
+                </td>
+                <td>
+                    <label class="formLabel">אימייל</label>
+                </td>
+                <tr>
+                    <td>
+                        <button id="viewPartButton" onclick="alert('U CLICKED ME');">משתתפים</button>
+
+                        <button id="signButton" onclick="alert('U CLICKED ME');">הרשם לאימון</button>
+                    </td>
+                </tr>
+            </tr>
+        </table>
+
+
         <h2>אימוני עבר <hr class="style-one" /></h2>
-        NEED TO BE IMPLEMENTED
+        <br/>
+    
+        <p>
+            <input type="hidden" name="GreetingAll" id="GreetingAll" value="Hello Everyone!" />
+            <input type="submit" id="PleasePushMe" />
+            <div id="test-div1">
+            </div>
+        </p>
+
+
     </div>
 </div>
 <div class="col span_2_of_3" style="margin:0;">
