dd633ab55c423d5d0a4885178366399cbed623c9
aviadhahami <aviadhahami@gmail.com>
2014-08-02 15:30:28 +0300

    Done the blog.php page + its css

--- blog.php
+++ blog.php
@@ -1,15 +1,45 @@
+<div class="span_3_of_3">
+
+    <img src="<?php bloginfo('template_url'); ?>/img/blogbg.jpg" class="blogHeader" />
+    <h1 id="blogHeadTitle"> WORKOUT OF THE DAY </h1>
+</div>
+<?php while (have_posts()) : the_post() ?>
+<div class="col span_1_of_3 ">
+    <div class="span_2_of_3 signup">
+        <h2>הרשמה לאימונים <hr class="style-one" /></h2>
+        <input type="text" value="" id="name">שם
+        <br />
+        <input type="text" value="" id="phone">טלפון
+        <br />
+        <input type="text" value="" id="mail">אימייל
+        <br />
+        <button id="viewPartButton" onclick="alert('U CLICKED ME');">משתתפים</button>
+        <button id="signButton" onclick="alert('U CLICKED ME');">הרשם לאימון</button>
+        <br />
+        <br />
+        <h2>אימוני עבר <hr class="style-one" /></h2>
+        NEED TO BE IMPLEMENTED
+    </div>
+</div>
+<div class="col span_2_of_3" style="margin:0;">
+    <h1 id="blogTitle">
+        <?php the_title(); ?>
+            <hr class="style-one" />
+    </h1>
+    <div class="col span_2_of_2 blogData">
+        <?php the_content(); ?>
+    </div>
+
+</div>
 
 
-    <?php while (have_posts()) : the_post() ?>
-    <h1 class="entry-title"><a href="<?php the_permalink(); ?>"
-  title="<?php printf(__('Go to %s', 'your-theme'), the_title_attribute('echo=0')); ?>"
-  rel="bookmark"><?php the_title(); ?></a></h1>
 
-    <?php the_content(); ?>
-    <?php previous_post( '&laquo; &laquo; %', '', 'yes'); ?>|
-    <?php next_post( '% &raquo; &raquo; ', '', 'yes'); ?>
 
-    <?php break; ?>
-    <?php endwhile; ?>
 
-    
+
+
+<?php previous_post( '&laquo; &laquo; %', '', 'yes'); ?>|
+<?php next_post( '% &raquo; &raquo; ', '', 'yes'); ?>
+
+<?php break; ?>
+<?php endwhile; ?>
