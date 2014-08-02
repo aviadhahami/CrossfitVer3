238bdbac27a941b521ec855a44be47bbb7a02010
aviadhahami <aviadhahami@gmail.com>
2014-08-03 01:48:30 +0300

    Manual revert
    hope it fixed it

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,5 +1,4 @@
 "use strict";
-
 $(document).ready(function() {
     $('body').css('display', 'none');
     $('body').fadeIn(1000);
@@ -7,11 +6,6 @@ $(document).ready(function() {
     console.log("JQ LOADED");
     // fakeCarousel();
     setInterval(function() {
-
-$(document).ready(function () {
-    console.log("JQ LOADED");
-    // fakeCarousel();
-    setInterval(function () {
         var i = $('#carImg').attr("src").charAt($('#carImg').attr("src").length - 5);
         var index = parseInt(i);
         //console.log(index);
@@ -25,161 +19,16 @@ $(document).ready(function () {
         $('#carImg').animate({
             opacity: 0
         }, 500, function() {
-
-        // console.log($('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png");
-
-
-        var finalStr = $('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png";
-
-        /** $('#carImg').fadeOut(500, function () {
-            $('#carImg').attr('src', finalStr);
-            $('#carImg').fadeIn(500);
-        });**/
-
-
-        $('#carImg').animate({
-            opacity: 0
-        }, 500, function () {
-
             $('#carImg').attr('src', finalStr);
             $('#carImg').animate({
                 opacity: 1
             }, 500);
 
-
         });
-
-
-        });
-
-    }, 30000);
-
-
-    function fakeCarouselTwo() {
-        var i = $('#carImg').attr("src").charAt($('#carImg').attr("src").length - 5);
-        var index = parseInt(i);
-        console.log(index);
-        if (index === 5) {
-            i = "1";
-        } else {
-            index++;
-            i = index.toString();
-            console.log($('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png");
-            var finalStr = $('#carImg').attr("src").substring(0, $('#carImg').attr("src").length - 5) + i + ".png";
-            $('#carImg').attr('src', finalStr);
-        }
-    }
-
-
-
-
 
     }, 30000);
 
    
-
-    function fakeCarousel() {
-
-        $('#carImg').fadeOut(500, function () {
-            $('#carImg').attr("src", "img/headerPhoto1.png");
-            $('#carImg').fadeIn(500);
-        });
-
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto2.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 30000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto3.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 60000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto4.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 90000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto5.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 120000);
-        setTimeout(fakeCarousel, 150000);
-    }
-
-    function fakeCarousel() {
-
-
-        $('#carImg').fadeOut(500, function () {
-            $('#carImg').attr("src", "img/headerPhoto1.png");
-            $('#carImg').fadeIn(500);
-        });
-
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto2.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 30000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto3.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 60000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto4.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 90000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto5.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 120000);
-        setTimeout(fakeCarousel, 150000);
-    }
-
-    function fakeCarousel() {
-
-        $('#carImg').fadeOut(500, function () {
-            $('#carImg').attr("src", "img/headerPhoto1.png");
-            $('#carImg').fadeIn(500);
-        });
-
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto2.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 30000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto3.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 60000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto4.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 90000);
-        window.setTimeout(function () {
-            $('#carImg').fadeOut(500, function () {
-                $('#carImg').attr("src", "img/headerPhoto5.png");
-                $('#carImg').fadeIn(500);
-            });
-        }, 120000);
-        setTimeout(fakeCarousel, 150000);
-    }
 
 
 });
