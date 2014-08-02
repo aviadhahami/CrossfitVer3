aa62db8ef69e26d5d0196f4739ab231c23159a11
aviadhahami <aviadhahami@gmail.com>
2014-08-03 02:02:55 +0300

    Fixed the stupid revert.
    Jesus fucking christ

--- landing.php
+++ landing.php
@@ -1,4 +1,9 @@
-﻿ 
+ 
+<?
+if (isset($the_post())){
+    echo '<script>alert("yes");';
+}
+?>
 <div class="carousel">
         <img alt="pic 1" id="carImg" class="carImg" src="<?php bloginfo('template_url'); ?>/img/headerPhoto1.png" />
     </div>
