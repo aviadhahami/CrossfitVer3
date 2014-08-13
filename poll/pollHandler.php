e07b2f0ce6677d9b9b31f53437cd21dafb5a06d4
aviadhahami <aviadhahami@gmail.com>
2014-08-13 23:48:42 +0300

    pollHandler.php
    Fixed the XML file creation and input !
    FUCK YEAH !

--- poll/pollHandler.php
+++ poll/pollHandler.php
@@ -1,42 +1,53 @@
-<? 
-    $name=$_GET[ "name" ];
-    $selection=$_GET[ "selection"]; 
-   // echo $selection;
+<?
+
+$selection = $_GET["selection"];
+$clientName = $_GET["name"];
 
 
-/**
-    $xml = new SimpleXMLElement('<xml/>');
+date_default_timezone_set("Israel");
+$dateNumber = date("dmy");
 
-    $participant = $xml->addChild('participant');
-    $participant->addChild('name', "aviad");
-    $participant->addChild('wod', $selection );
-    Header('Content-type: text/xml');
-    print($xml->asXML());
-**/
+if (file_exists("pollData/" . $dateNumber . "-Wod.xml")) {
+    echo("FILE IS THERE");
+    $file = "pollData/" . $dateNumber . "-Wod.xml";
+    $xml = simplexml_load_file($file);
 
-$xml = new DOMDocument("1.0");
+    //$data = $xml->data;
 
-$root = $xml->createElement("data");
-$xml->appendChild($root);
+    $newParticipant = $xml->addChild('participant');
+    $newParticipant->addChild('name', $clientName);
+    $newParticipant->addChild('wod', $selection);
 
-$name   = $xml->createElement("name");
-$nameText = $xml->createTextNode("$name");
-$name->appendChild($nameText);
+    $xml->asXML($file);
+} else {
+    echo("FILE NOT THERE");
+    $xml = new DOMDocument("1.0");
 
-$wod   = $xml->createElement("wod");
-$wodText = $xml->createTextNode("$selection");
-$wod->appendChild($wodText);
+    $root = $xml->createElement("data");
+    $xml->appendChild($root);
+
+    $name = $xml->createElement("name");
+    $nameText = $xml->createTextNode("$clientName");
+    $name->appendChild($nameText);
+
+    $wod = $xml->createElement("wod");
+    $wodText = $xml->createTextNode("$selection");
+    $wod->appendChild($wodText);
 
 
-$participant = $xml->createElement("participant");
-$participant->appendChild($name);
-$participant->appendChild($wod);
+    $participant = $xml->createElement("participant");
+    $participant->appendChild($name);
+    $participant->appendChild($wod);
 
-$root->appendChild($participant);
+    $root->appendChild($participant);
 
-$xml->formatOutput = true;
-echo "<xmp>". $xml->saveXML() ."</xmp>";
+    $xml->formatOutput = true;
+    echo "<xmp>" . $xml->saveXML() . "</xmp>";
 
-$xml->save("myWod.xml") or die("Error");
+    $xml->save("pollData/" . $dateNumber . "-Wod.xml") or die("Error");
+
+}
+/** The following part generates a full XML file including headers*/
+
 
 ?>
