08ab99ee1c3a343ca9edeffaaa354c43fdd7ff57
aviadhahami <aviadhahami@gmail.com>
2014-08-25 15:49:20 +0300

    some uncaught changes

---
+++ about1.php
@@ -0,0 +1,143 @@
+<div class="containerScroller">
+    <div class="splash_container">
+        <div class="top_container">
+            <div class="span_3_of_3">
+                <img src="<?php bloginfo('template_url'); ?>/img/blogbg.jpg" class="blogHeader"/>
+
+                <h1 id="blogHeadTitle"> ABOUT US </h1>
+            </div>
+        </div>
+        <div class="content">
+            <div class="blogger">
+                <div class="col_side left_span"></div>
+                <div class="midspace_span"></div>
+                <div class="col_side right_span">
+                    <div class="superheader">
+                        Main page
+                    </div>
+                </div>
+            </div>
+            <div class="blogger">
+                <div class="col_side left_span">
+                    <div class="span_2_of_3 signup">
+                        <h2> Workouts signup
+                            <hr class="style-one"/>
+                        </h2>
+                        <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic2.jpg);"
+                             class="blockImg"/>
+
+                        <div class="generalText">
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
+                    </div>
+
+                    <h2>אימוני עבר </h2>
+                    <hr class="style-one"/>
+                    <div class="recentPosts">
+                        <div class="recentPostSingle">
+                            <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic2.jpg);"
+                                 class="recentPostImg"/>
+
+                            <div class="generalText">
+                                <span class="recentPostTextHeader">Nigga nigga nigga</span>
+                                <br> try one two three
+                            </div>
+                        </div>
+
+                        <div class="recentPostSingle">
+                            <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic2.jpg);"
+                                 class="recentPostImg"/>
+
+                            <div class="generalText">
+                                <span class="recentPostTextHeader">Nigga nigga nigga</span>
+                                <br> try one two three
+                            </div>
+                        </div>
+
+                        <div class="recentPostSingle">
+                            <img style="background-image:url(<?php bloginfo('template_url'); ?>/img/mpic2.jpg);"
+                                 class="recentPostImg"/>
+
+                            <div class="generalText">
+                                <span class="recentPostTextHeader">Nigga nigga nigga</span>
+                                <br> try one two thee
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="midspace_span"></div>
+
+
+                <div class="col_side right_span">
+                    <h2>הרשמה לאימונים
+                        <hr class="style-one"/>
+                    </h2>
+
+                    <div class="col_side span_2_of_2">
+
+                        <div class="generalText">
+                            <br>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
+
+                        <h2 style="margin-top: 30px;">הרשמה לאימונים </h2>
+                        <hr class="style-one"/>
+
+                        <dl class="list">
+                            <div style="width: 50%;">
+                                <dt>
+                                <div class="listText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                    deserunt mollit anim id est laborum.
+                                </div>
+                                </dt>
+                            </div>
+
+                            <div style="width: 50%; float: left;">
+                                <dt>
+                                <div class="listText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                    deserunt mollit anim id est laborum.
+                                </div>
+                                </dt>
+                            </div>
+
+                            <div style="width: 100%;">
+                                <dt>
+                                <div class="listText">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                    deserunt mollit anim id est laborum.
+                                </div>
+                                </dt>
+                            </div>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
